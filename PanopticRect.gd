4bcaab44c52e96ac12e28f21ec9dd392b71d5cbc
Andy Barcia Rodríguez <andybarcia4@gmail.com>
2026-02-23 14:15:56 +0100

    Refactor bbox math into overlay and export styling

--- PanopticRect.gd
+++ PanopticRect.gd
@@ -2,8 +2,17 @@ extends ZoomPanRect
 
 signal segment_clicked(segment_id: int, append_selection: bool, remove_selection: bool)
 
+var selected_ids: Array[int] = []
+var segment_bounds_uv: Dictionary = {}
+var bbox_overlay: BoundingBoxOverlay
+
 func _ready() -> void:
 	super()
+	bbox_overlay = BoundingBoxOverlay.new()
+	bbox_overlay.name = "BoundingBoxOverlay"
+	bbox_overlay.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
+	bbox_overlay.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	add_child(bbox_overlay)
 	set_selected_ids([])
 
 func _gui_input(event: InputEvent) -> void:
@@ -21,6 +30,10 @@ func set_selected_id(id: int) -> void:
 	set_selected_ids([id])
 
 func set_selected_ids(ids: Array[int]) -> void:
+	selected_ids = ids.duplicate()
+	if bbox_overlay != null:
+		bbox_overlay.set_selected_ids(selected_ids)
+
 	var shader_ids := PackedFloat32Array()
 	for id in ids:
 		shader_ids.append(float(id))
@@ -68,7 +81,22 @@ func update_shader_image(image, panoptic_image) -> void:
 
 	# Use the panoptic image for UV calculations
 	set_reference_image(panoptic_image)
+	segment_bounds_uv = bbox_overlay.compute_segment_bounds_uv(panoptic_image)
+	bbox_overlay.set_segment_bounds_uv(segment_bounds_uv)
 
 	_update_shader_control_size()
 	_update_shader_zoompan()
 	set_selected_ids([])
+
+func _update_shader_control_size() -> void:
+	super._update_shader_control_size()
+	_update_overlay_view()
+
+func _update_shader_zoompan() -> void:
+	super._update_shader_zoompan()
+	_update_overlay_view()
+
+func _update_overlay_view() -> void:
+	if bbox_overlay == null:
+		return
+	bbox_overlay.set_view(reference_image, pan, zoom)
